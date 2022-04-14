4bbbae2b35fb4024223e65ca78550eaf20f13919
BenjaminCarmenate <79183964+BenjaminCarmenate@users.noreply.github.com>
2022-04-13 21:41:46 -0400

    Initial Commit

---
+++ CDA3103/project.c
@@ -0,0 +1,122 @@
+#include "spimcore.h"
+
+
+/* ALU */
+/* 10 Points */
+void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
+{
+  unsigned Z;
+  *Zero = 1;
+
+  switch (ALUControl)
+  {
+    case 0b000:
+      Z = A + B;
+      *ALUresult = Z;
+      break;
+    case 0b001:
+      Z = A + B;
+      *ALUresult = Z;
+      break;
+    case 010:
+      Z = 0;
+      if(A < B)
+        Z = 1;
+      *ALUresult = Z;
+      break;
+    case 011:
+      Z = 0;
+      if(A < B)
+        Z = 1;
+      *ALUresult = Z;
+      break;
+    case 100:
+      Z = A & B;
+      *ALUresult = Z;
+      break;
+    case 101:
+      Z = A | B;
+      *ALUresult = Z;
+      break;
+    case 110:
+      Z = B << 16;
+      *ALUresult = Z;
+      break;
+    case 111:
+      Z = ~A;
+      *ALUresult = Z;
+      break;
+  }
+
+  if(*ALUresult == 0)
+    *Zero = 0;
+}
+
+/* instruction fetch */
+/* 10 Points */
+int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
+{
+  *instruction = Mem[(PC>>2)];
+  return 0;
+}
+
+
+/* instruction partition */
+/* 10 Points */
+void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
+{
+
+}
+
+
+
+/* instruction decode */
+/* 15 Points */
+int instruction_decode(unsigned op,struct_controls *controls)
+{
+
+}
+
+/* Read Register */
+/* 5 Points */
+void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
+{
+
+}
+
+
+/* Sign Extend */
+/* 10 Points */
+void sign_extend(unsigned offset,unsigned *extended_value)
+{
+
+}
+
+/* ALU operations */
+/* 10 Points */
+int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
+{
+
+}
+
+/* Read / Write Memory */
+/* 10 Points */
+int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
+{
+
+}
+
+
+/* Write Register */
+/* 10 Points */
+void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
+{
+
+}
+
+/* PC update */
+/* 10 Points */
+void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
+{
+
+}
